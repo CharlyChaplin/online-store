5c8a0ef14ad5369fbc9c0d79aa7f1c1ec8cf0128
CharlyChaplin <charlychaplin83@gmail.com>
2022-07-15 22:40:54 +0700

    [BACKEND] - Middleware, проверяющий авторизован ли пользователь

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
+
+const { SECRET_KEY } = config().parsed;
+
+export default (req, res, next) => {
+	if (req.method === 'OPTIONS') next();
+
+	try {
+		const token = req.headers.authorization.split(' ')[1];
+		if (!token) res.status(401).json({ message: "User not authorized" });
+
+		const decoded = jwt.verify(token, SECRET_KEY);
+		//добавим в запрос данные, которые были вытащены из токена
+		req.user = decoded;
+		next();
+
+	} catch (err) {
+		res.status(401).json({ message: "User not authorized" })
+	}
+}
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import UserController from "../controllers/userController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 
 const router = new Router();
@@ -8,7 +9,7 @@ const router = new Router();
 router.post('/registration', UserController.registration);
 router.post('/login', UserController.login);
 //авторизован ли пользователь
-router.get('/auth', UserController.check);
+router.get('/auth', authMiddleware, UserController.check);
 
 
 
