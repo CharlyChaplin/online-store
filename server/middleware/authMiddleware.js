72cd0b54d4305992b047efe00bab854678658c6b
CharlyChaplin <charlychaplin83@gmail.com>
2022-07-15 23:10:51 +0700

    [BACKEND] - Middleware, проверяющий роль пользователя

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -5,7 +5,6 @@ const { SECRET_KEY } = config().parsed;
 
 export default (req, res, next) => {
 	if (req.method === 'OPTIONS') next();
-
 	try {
 		const token = req.headers.authorization.split(' ')[1];
 		if (!token) res.status(401).json({ message: "User not authorized" });
@@ -14,8 +13,7 @@ export default (req, res, next) => {
 		//добавим в запрос данные, которые были вытащены из токена
 		req.user = decoded;
 		next();
-
 	} catch (err) {
-		res.status(401).json({ message: "User not authorized" })
+		res.status(403).json({ message: "Not authorized" });
 	}
 }
---
+++ server/middleware/checkRoleMiddleware.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
+
+const { SECRET_KEY } = config().parsed;
+
+export default (role) => {
+	return (req, res, next) => {
+		if (req.method === 'OPTIONS') next();
+
+		try {
+			const token = req.headers.authorization.split(' ')[1];
+			
+			if (!token) res.status(401).json({ message: "User not authorized" });
+
+			const decoded = jwt.verify(token, SECRET_KEY);
+
+			console.log('DECODED.ROLE = ', decoded.role, 'ROLE = ', role);
+
+			if (decoded.role !== role) return res.json({ message: "Access denied" });
+
+			//добавим в запрос данные, которые были вытащены из токена
+			req.user = decoded;
+			next();
+		} catch (err) {
+			res.status(401).json({ message: "Not authorizated" })
+		}
+	}
+}
