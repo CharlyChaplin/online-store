a0dbaca0a21a00ca22a1c36bcac682e1df3f0c55
crash <charlychaplin83@gmail.com>
2022-07-18 13:03:01 +0700

    [FRONTEND] - Начало, структура приложения

---
+++ client/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './init/main.sass';
+
+ReactDOM.createRoot(document.getElementById('wrapper'))
+
+.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
